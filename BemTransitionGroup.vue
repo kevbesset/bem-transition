04c465e716aaf8e143d877bfb9b06f5cb6a828c0
kevbesset <kevbesset@gmail.com>
2021-01-20 11:07:08 +0100

    Create bem-transition-group component

---
+++ BemTransitionGroup.vue
@@ -0,0 +1,20 @@
+<script>
+import { h, Transition } from "vue";
+
+export default {
+  name: "BemTransition",
+  props: {
+    name: { type: String },
+  },
+  render() {
+    return h(
+      Transition,
+      {
+        ...this.$attrs,
+        name: this.name ? `${this.name}-` : this.name,
+      },
+      this.$slots
+    );
+  },
+};
+</script>
